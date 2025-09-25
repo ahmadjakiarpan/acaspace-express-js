7449014a34298afe91ff98ff0cb5cf2905ef3868
ahmadjakiarpan <ahmadjakii462@gmail.com>
2025-09-25 20:54:56 +0700

    Update Router Product

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getProducts } from "../controllers/product.controller";
+import { addProduct, deleteProduct, editProduct, getProductById, getProducts } from "../controllers/product.controller";
 
 
 const router = Router()
 
 router.get("/", getProducts)
+router.get("/:id", getProductById)
+router.post("/", addProduct)
+router.put("/:id", editProduct)
+router.delete("/:id", deleteProduct)
 
 export default router
