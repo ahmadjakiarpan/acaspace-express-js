a902393510c4182628eb52122ccad77c1bd92649
ahmadjakiarpan <ahmadjakii462@gmail.com>
2025-09-25 20:51:56 +0700

    Update Controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -21,3 +21,72 @@ export const getProducts = async (req: Request, res: Response) => {
         res.status(500).json ({meassage: "Error get product", err})
     }
 }
+
+export const getProductById = async (req: Request, res: Response) => {
+    try {
+        if (!req.params.id) {
+            return res.status(400).json({message: "Wajib ada ID"})
+        }
+        const id = parseInt(req.params.id)
+        const product = await ProductModel.getById(id)
+
+        if (!product) {
+            return res.status(404).json({message: "Product not found"})
+        }
+
+        res.status(200).json({
+            message: "Get Product Succesfully",
+            result: product
+        })
+    } catch (err) {
+        res.status(500).json({message: "Error Get Product", err})
+    }
+}
+
+export const addProduct = async (req: Request, res: Response)  => {
+    try {
+        const newProduct = await ProductModel.createProduct(req.body)
+        res.status(201).json({
+            statusCode: 201,
+            message: "Product Created Succesfully",
+            results: newProduct
+        })
+    } catch (err) {
+        res.status(500).json({error: "Error Create Product", message: err})
+    }
+}
+
+export const editProduct = async (req: Request, res: Response) => {
+    try {
+        if (!req.params.id) {
+            return res.status(400).json({message: "Wajib ada ID"})
+        }
+        const id = parseInt(req.params.id)
+        const updateProdct = await ProductModel.updateProduct(id, req.body)
+        res.status(201).json({
+            statusCode: 201,
+            message: "Data Updated Succesfully",
+            results: updateProdct
+        })
+    } catch (err) {
+        res.status(500).json({error: "Error Update Product", message: err})
+    }
+    
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        if (!req.params.id) {
+            return res.status(400).json({message: "Wajib ada ID"})
+        }
+        const id = parseInt(req.params.id)
+        const deleted = await ProductModel.deleteProduct(id)
+
+        res.status(200).json({
+            message: "Product Delete Succesfully",
+            data: deleted,
+        })
+    } catch (err) {
+        res.status(500).json({message: "Error Get Product"})
+    }
+}
